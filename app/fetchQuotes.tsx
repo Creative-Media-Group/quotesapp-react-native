bf2bc010f5281a61d12d201ca4672c0f599763a2
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-29 18:41:22 +0200

    .

--- app/fetchQuotes.tsx
+++ app/fetchQuotes.tsx
@@ -1,23 +1,21 @@
-export default function fetchQuotes(API_KEY: string) {
-    async (apiKey: any) => {
-        try {
-            const response = await fetch(`https://api.api-ninjas.com/v1/quotes`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Api-Key': apiKey,
-                },
-            });
+export default async function fetchQuotes(API_KEY: string) {
+    try {
+        const response = await fetch(`https://api.api-ninjas.com/v1/quotes`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Api-Key': API_KEY,
+            },
+        });
 
-            if (!response.ok) {
-                throw new Error("Failed to fetch data");
-            }
-
-            const json = await response.json(); // Die API-Antwort wird geparsed
-            return json; // Rückgabe der JSON-Daten
-        } catch (error) {
-            console.error("Error fetching data:", error.message);
-            throw error; // Fehler weitergeben, um sie in der aufrufenden Funktion zu behandeln
+        if (!response.ok) {
+            throw new Error("Failed to fetch data");
         }
-    };
+
+        const json = await response.json(); // Die API-Antwort wird geparsed
+        return json; // Rückgabe der JSON-Daten
+    } catch (error: any) {
+        console.error("Error fetching data:", error.message);
+        throw error; // Fehler weitergeben, um sie in der aufrufenden Funktion zu behandeln
+    }
 }
